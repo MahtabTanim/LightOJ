ea76bb4ffc59ca246f55a7829cda413975bd0c6c
MahtabTanim <63159321+MahtabTanim@users.noreply.github.com>
2020-04-09 23:27:58 +0600

    Create 1001.cpp

---
+++ 1001.cpp
@@ -0,0 +1,18 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    int main()
+    {
+       
+        int n,i,j;
+        cin>>n;
+    while(n--)
+    {
+    cin>>i;
+    if(i<=10)cout<<i<<" "<<0;
+     else{
+    cout<<10<<" "<<i-10;
+    }
+    cout<<endl;
+    }
+        return 0;
+    }
